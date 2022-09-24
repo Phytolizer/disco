b36bee9791e1c4d93f9722d2a8b7c28ce860484b
Gehrig Wilcox <4042887+gehrigwilcox@users.noreply.github.com>
2022-09-24 02:23:55 -0500

    Made methods abstract

--- disco/transformable.py
+++ disco/transformable.py
@@ -3,17 +3,11 @@ from numpy import array
 
 
 class Transformable:
-<<<<<<< HEAD
     def __init__(self,x,y,width,height,theta):
         self.transform = array( [x,y],
                                 [width,height],
                                 [theta,0])
-    
-    def getTransform(self):
-        return self.transform
-=======
     @property
     @abstractmethod
     def transform(self) -> array:
         pass
->>>>>>> 1bc7b3c301b5f04bab9e911625e30515be7702d5
